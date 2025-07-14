110fab03e87c9261e3ba5edb672e2e007f4bd4df
saranyababu1991 <saranya.naresh.uk@gmail.com>
2025-07-14 23:34:09 +0100

    Delete User-Management directory

--- User-Management/Security-Compliance/MFA-Status-Report.ps1
+++
@@ -1,7 +0,0 @@
-# Requires MSOnline module
-Import-Module MSOnline
-Connect-MsolService
-
-Get-MsolUser -All | Select DisplayName, UserPrincipalName, `
-@{Name="MFAStatus";Expression={$_.StrongAuthenticationRequirements.State}} |
-Export-Csv "./MFA_Status_Report.csv" -NoTypeInformation
--- User-Management/Security-Compliance/Reports/License-Usage-Report.ps1
+++
@@ -1,5 +0,0 @@
-Connect-MsolService
-
-Get-MsolUser -All | Where-Object {$_.isLicensed -eq $true} |
-Select DisplayName, UserPrincipalName, Licenses |
-Export-Csv "./Licensed_Users.csv" -NoTypeInformation
